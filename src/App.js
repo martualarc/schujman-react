aa0712679e8bc8734a49125920d60bd145d83c5a
martualarc <martina.alarcon029@gmail.com>
2023-10-06 15:28:26 -0300

    fondo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function Boton (props) {
     const [cont, setCont] = useState(0);
 
     return (
-        <div className='flex flex-col items-center mx-20'>
+        <div className='flex flex-col items-center mx-20 bg-slate-700'>
         <button 
          className='text-white rounded-lg p-2 text-xl bg-orange-800 hover:bg-white hover:text-orange-800' 
             onClick={() => (setCont(props.suma?cont + 1:cont-1))}>
